4db5340ff1aadc8719f4aa3a84b354919b4c2fb0
Nataly Angulo <nataly.angulo530@gmail.com>
2021-01-20 18:36:59 +0000

    inital upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function trafficLight() {
+    var red = document.getElementById("red").style.backgroundColor= "rgb(255, 0, 0)";
+    var yellow = document.getElementById("orange").style.backgroundColor= " rgb(255, 187, 0)";
+    var green = document.getElementById("green").style.backgroundColor= "rgb(34, 160, 34)";
+} 
+
+setTimeout(function(){
+
+}, 3000);
